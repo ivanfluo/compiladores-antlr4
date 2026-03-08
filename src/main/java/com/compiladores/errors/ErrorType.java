8acb6edddb02d663756024bd545755f9b52e6771
ivanfluo <ivanfluo@gmail.com>
2026-03-08 15:44:01 -0600

    Ivan Hernandez: Creación de enum base para los tipos de errores

--- src/main/java/com/compiladores/errors/ErrorType.java
+++ src/main/java/com/compiladores/errors/ErrorType.java
@@ -1,4 +1,21 @@
 package com.compiladores.errors;
 
-public class ErrorType {
+// [Inicio][8/03/2026][Ivan Hernandez][Tarjeta de Jira: https://miumg-team-compi.atlassian.net/browse/SCRUM-9][Implementación de la clase para los tipos de errores]
+
+/**
+ * Enumeración para los tipos de errores que pueden ocurrir durante el proceso de análisis.
+ * @author Ivan Hernandez
+ */
+public enum ErrorType {
+    /**
+     * Error léxico detectado por el lexer.
+     */
+    LEXICAL,
+
+    /**
+     * Error sintáctico detectado por el parser.
+     */
+    SYNTAX
 }
+
+// [Fin][8/03/2026][Ivan Hernandez][Tarjeta de Jira: https://miumg-team-compi.atlassian.net/browse/SCRUM-9][Implementación de la clase para los tipos de errores]
