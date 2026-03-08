5d8ae21ef13fc1c5f2814f455ad21d57a74c7bbd
ivanfluo <ivanfluo@gmail.com>
2026-03-08 15:57:41 -0600

    Ivan Hernandez:Creación de la interfaz para el desacoplamiento de los metodos para capturar errores y generar el reporte.

--- src/main/java/com/compiladores/errors/IErrorLogger.java
+++ src/main/java/com/compiladores/errors/IErrorLogger.java
@@ -1,4 +1,32 @@
 package com.compiladores.errors;
+// [Inicio][8/03/2026][Ivan Hernandez][Tarjeta de Jira: https://miumg-team-compi.atlassian.net/browse/SCRUM-9][Interfaz que permite desacoplar la lógica para el registro de errores.]
 
+/**
+ * Define el contrato para el registro de errores.
+ * @author Ivan Hernandez
+ */
 public interface IErrorLogger {
+
+    /**
+     * Registra el error léxico.
+     * @param line línea donde ocurrió el error
+     * @param column columna donde ocurrió el error
+     * @param message descripción del error
+     */
+    void logLexicalError(int line, int column, String message);
+
+    /**
+     * Registra el error sintactico.
+     * @param line línea donde ocurrió el error
+     * @param column columna donde ocurrió el error
+     * @param message descripción del error
+     */
+    void logSyntaxError(int line, int column, String message);
+
+    /**
+     * Genera el reporte de errores.
+     * @param path ruta donde se generará el archivo de reporte
+     */
+    void generateReport(String path);
 }
+// [Fin][8/03/2026][Ivan Hernandez][Tarjeta de Jira: https://miumg-team-compi.atlassian.net/browse/SCRUM-9][Interfaz que permite desacoplar la lógica para el registro de errores.]
